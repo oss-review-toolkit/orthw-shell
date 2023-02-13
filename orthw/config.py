854163f90454724fb245a7eaf534d5cf9e45faa8
Helio Chissini de Castro <heliocastro@gmail.com>
2023-02-13 14:11:07 +0100

    feat(Config): Create initial config class
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/config.py
+++ orthw/config.py
@@ -2,38 +2,72 @@
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
 from pathlib import Path
-from typing import Any, List
-
+from typing import Dict
 import logging
+import sys
+
 import yaml
-
-
-defaults = {"test": "test"}
+from dict2obj import Dict2Obj
 
 
 class Config:
     """Base config object. Reads default orthw config file or set default"""
 
+    _ortproject_dir: Path = Path.home() / "ort-project"
+
+    _orthwconfig_default = {
+        "configuration_home": (_ortproject_dir / "/ort-project/config").as_posix(),
+        "ort_home": (_ortproject_dir / "ort").as_posix(),
+        "scancode_home": (_ortproject_dir / "scancode-toolkit").as_posix(),
+        "exports_home": (_ortproject_dir / "exports").as_posix(),
+        "orthw_home": (_ortproject_dir / "orthw").as_posix(),
+    }
+
     # Default config file
-    _config: List[Any] | None = None
-    _configfile: Path = Path.home() / ".config" / ".orthw"
+    _config: Dict[str, str] = Dict2Obj(_orthwconfig_default)
+    _configfile: Path = Path.home() / ".config" / "orthw"
+    _project_default: Path = Path.home() / "ort-project"
+    dotdir: Path = Path.home() / ".orthw"
+
+    _log = logging.getLogger("rich")
 
     def __init__(self, configfile: str | None = None) -> None:
-        # Set default if no ccustom config file is passed
+        # Point the main configfile to custom
         if configfile:
             self._configfile = Path(configfile)
 
-    def __readconfig(self) -> bool:
+        self.__readconfig()
+
+    def __readconfig(self) -> None:
         try:
             with open(self.configfile, "r") as yamlconfig:
                 self._config = yaml.safe_load(yamlconfig)
         except IOError:
-            logging.error(
-                "Missing the required configuration file '$orthw_config_file'."
-                "Please create that file following the installation instructions, see "
+            self._log.warning(
+                f"Missing the required configuration file {self._configfile}.\n"
+                "The default config file will be created. Please customize it according to\n"
                 "https://github.com/oss-review-toolkit/orthw#3-create-your-orthw-configuration."
             )
-        return False
+            # Generate the default config file
+            Path.mkdir(Path.home() / ".config", exist_ok=True)
+            try:
+                with open(self._configfile, "w") as yamlconfig:
+                    yaml.dump(self._orthwconfig_default, yamlconfig)
+            except IOError:
+                self._log.error(f"Can't create the default config file {self._configfile} !")
+                sys.exit(1)
+
+    def get(self, config_entry: str) -> str | None:
+        """Return the value of the configured key
+
+        :param config_entry: Desired config parameter
+        :type config_entry: str
+        :return: Configured value
+        :rtype: str
+        """
+        if config_entry in self._config:
+            return self._config[config_entry]
+        return None
 
     @property
     def configfile(self) -> str:
