1432b02dad51b07faa3d1c52a122207a9156d5d1
Helio Chissini de Castro <heliocastro@gmail.com>
2023-02-13 17:33:06 +0100

    feat(command): First functional command - clean
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/commands/clean.py
+++ orthw/commands/clean.py
@@ -1,25 +1,40 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
-import logging
-
-import click
+import os
 
 from orthw.commandbase import CommandBase, command_group
+
 
 # ----------------------------------
 # Command Line options and arguments
 
 
-class CheckAdvisoriesCommand(CommandBase):
+class CleanCommand(CommandBase):
     """orthw commnand - clean"""
 
-    _command_name: str = "check-advisories"
+    _command_name: str = "clean"
+
+    def __init__(self) -> None:
+        pass
+
+    def clean(self) -> None:
+        try:
+            dotdir = self.config.get("dotdir")
+            if dotdir and dotdir.is_dir():
+                os.removedirs(dotdir)
+                self.log.info(f"Removed directory {dotdir}")
+        except OSError:
+            self.log.error(f"Removing directory {dotdir}")
+        try:
+            config_file = self.config.get("repository_configuration_file")
+            if config_file and config_file.is_file():
+                config_file.unlink(missing_ok=True)
+                self.log.info(f"Removed file {config_file}")
+        except OSError:
+            self.log.error(f"Error removing directory {self.config.get('repository_configuration_file')}")
 
 
 @command_group.command()
-@click.option("-d", "--debug", is_flag=True)
-def check_advisories(debug: bool) -> None:
-    if debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-        CheckAdvisoriesCommand()
+def clean() -> None:
+    CleanCommand().clean()
