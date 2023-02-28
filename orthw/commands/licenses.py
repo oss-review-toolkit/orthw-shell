ed60e2a6bafe92587a644daee08a53978a2627a4
Helio Chissini de Castro <heliocastro@gmail.com>
2023-02-28 19:35:37 +0100

    feat(command): Add license command
    Both license commands now are merged, with source-code-dir now is
    option, instead of positional parameters
    
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/commands/licenses.py
+++ orthw/commands/licenses.py
@@ -1,24 +1,67 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
-from rich import print
+import click
 
 from orthw.commandbase import CommandBase, command_group
+from orthw.utils.process import run
+
+#   package_id=$2
+#   require_initialized
+
+#   evaluate
+#   echo "Downloading sources for $2."
+#   orth list-licenses \
+#     --ort-file $evaluation_result_file \
+#     --package-id $package_id \
+#     --repository-configuration-file $repository_configuration_file \
+#     --package-configuration-dir $ort_config_package_configuration_dir \
+#     --apply-license-finding-curations \
+#     --omit-excluded
 
 
-# ----------------------------------
-# Command Line options and arguments
-
-
-class OrtHWCommand(CommandBase):
+class Command(CommandBase):
     """orthw command - licenses"""
 
     _command_name: str = "licenses"
 
-    def process(self) -> None:
-        print("\n[sandy_brown]This command is not implemented yet.[/sandy_brown]")
+    def licenses(self, package_id: str, source_code_dir: str | None = None) -> None:
+        """licenses
+
+        :param package_id: Id of package
+        :type package_id: str
+        """
+
+        args: list[str] = [
+            "ort",
+            "list-licenses",
+            "--package-id",
+            package_id,
+            "--apply-license-finding-curations",
+            "--omit-excluded",
+        ]
+
+        evaluation_result_file = self.config.get("evaluation_result_file")
+        if evaluation_result_file:
+            args += ["--ort-file", evaluation_result_file.as_posix()]
+
+        repository_configuration_file = self.config.get("repository_configuration_file")
+        if repository_configuration_file:
+            args += ["--repository-configuration-file", repository_configuration_file.as_posix()]
+
+        ort_config_package_configuration_dir = self.config.get("ort_config_package_configuration_dir")
+        if ort_config_package_configuration_dir:
+            args += ["--package-configuration-dir", ort_config_package_configuration_dir.as_posix()]
+
+        if source_code_dir:
+            args += ["--source-code-dir", source_code_dir]
+
+        # Execute external run
+        run(args=args, live_output=True)
 
 
 @command_group.command()
-def licenses() -> None:
-    OrtHWCommand().process()
+@click.option("--source-code-dir", default=None)
+@click.argument("package_id")
+def licenses(package_id: str, source_code_dir: str | None) -> None:
+    Command().licenses(package_id, source_code_dir=source_code_dir)
