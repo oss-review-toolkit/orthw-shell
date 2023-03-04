6b8acad3f8d428936eff7859f5f4fe96c590e1bb
Helio Chissini de Castro <heliocastro@gmail.com>
2023-03-04 12:04:41 +0100

    feat(utils): Add admin/root check function
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/utils/__init__.py
+++ orthw/utils/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
+import ctypes
+import os
 import logging as stdlogger
 
 from rich.console import Console
@@ -10,3 +12,13 @@ logging = stdlogger.getLogger("rich")
 
 # Global console output
 console = Console()
+
+
+def admin() -> bool:
+    # We not allow run as a root/admin
+    try:
+        is_admin = os.getuid() == 0
+    except AttributeError:  # We're playing Windows game
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0  # type: ignore
+
+    return is_admin
