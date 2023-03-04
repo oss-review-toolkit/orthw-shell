4ee03a0c2a1eb3c676df717b774a4c0b6bb228a8
Helio Chissini de Castro <heliocastro@gmail.com>
2023-03-04 12:50:06 +0100

    feat(command): Add copyrights command
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/commands/copyrights.py
+++ orthw/commands/copyrights.py
@@ -1,24 +1,82 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
-from rich import print
+from pathlib import Path
 
+import click
+
+from orthw import config
 from orthw.commandbase import CommandBase, command_group
+from orthw.utils import logging
+from orthw.utils.process import run
+from orthw.utils.required import require_initialized
 
 
-# ----------------------------------
-# Command Line options and arguments
+#   orth list-copyrights \
+#     --ort-file $scan_result_file \
+#     --package-configuration-dir $ort_config_package_configuration_dir \
+#     --copyright-garbage-file $ort_config_copyright_garbage_file \
+#     --show-raw-statements \
+#     > $copyrights_debug_file
+
+#   echo "Results written to: $copyrights_file, $copyrights_debug_file."
+#   exit 0
+# fi
 
 
-class OrtHWCommand(CommandBase):
+# if [ "$command" = "copyrights" ] && [ "$#" -eq 2 ]; then
+#   package_id=$2
+#   require_initialized
+
+#   orth list-copyrights \
+#     --ort-file $scan_result_file \
+#     --package-id $package_id
+
+#   exit 0
+# fi
+
+
+class Command(CommandBase):
     """orthw command - copyrights"""
 
     _command_name: str = "copyrights"
 
-    def process(self) -> None:
-        print("\n[sandy_brown]This command is not implemented yet.[/sandy_brown]")
+    def copyrights(self, package_id: str = "") -> None:
+        """OrtHW Command
+
+        :param package_id: Package ID, defaults to ""
+        :type package_id: str, optional
+        """
+
+        require_initialized()
+
+        ort_config_copyright_garbage_file: Path | None = config.get("ort_config_copyright_garbage_file")
+        ort_config_package_configuration_dir: Path | None = config.get("ort_config_package_configuration_dir")
+        scan_result_file: Path | None = config.get("scan_result_file")
+        if not scan_result_file or not ort_config_package_configuration_dir or not ort_config_copyright_garbage_file:
+            logging.error("Invalid configuration.")
+            return
+
+        args: list[str] = ["orth", "list-copyrights", "--ort-file", scan_result_file.as_posix()]
+
+        if package_id:
+            args += ["--package-id", package_id]
+            run(args=args)
+        else:
+            args += [
+                "--package-configuration-dir",
+                ort_config_package_configuration_dir.as_posix(),
+                "--copyright-garbage-file",
+                ort_config_package_configuration_dir.as_posix(),
+            ]
+
+            run(args=args, output_file=config.get("copyrights_file"))
+
+            args += ["--show-raw-statements"]
+            run(args=args, output_file=config.get("copyrights_debug_file"))
 
 
 @command_group.command()
-def copyrights() -> None:
-    OrtHWCommand().process()
+@click.argument("package-id", type=str, default="")
+def copyrights(package_id: str) -> None:
+    Command().copyrights(package_id)
--- orthw/utils/process.py
+++ orthw/utils/process.py
@@ -1,14 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
+
 import subprocess  # nosec
+import sys
+from pathlib import Path
 from typing import Any
 
-from orthw.utils import logging, console
+from orthw.utils import admin, logging, console
 from orthw.utils.required import required_command
 
 
-def run(args: list[str], live_output: bool = False) -> int | Any:
+def run(args: list[str], live_output: bool = False, output_file: Path | None = None) -> int | Any:
     """Run a process with defined arguments
 
     :param args: Arguments
@@ -18,6 +21,11 @@ def run(args: list[str], live_output: bool = False) -> int | Any:
     :return: Process resulting code
     :rtype: int | Any
     """
+
+    if admin():
+        logging.error("This scrip is not allowed to run as admin.")
+        sys.exit(1)
+
     # Expect first argument be the required command
     main_cmd = required_command(args[0])
     if not main_cmd:
@@ -28,19 +36,27 @@ def run(args: list[str], live_output: bool = False) -> int | Any:
 
     logging.debug(f"command line: [bright_green]{' '.join(args)}[/]", extra={"markup": True})
 
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # nosec
-    if live_output:
-        if proc.stdout:
-            while True:
-                output = proc.stdout.readline()
-                if proc.poll() is not None:
-                    break
-                if output:
-                    line = output.decode("utf-8").strip()
-                    # Avoid funny ort log output that ressemble markup closing tag
-                    if "[/" in line:
-                        line = line.replace("[", "").replace("]", "")
-                    console.print(line, style="bright_white")
+    if output_file:
+        try:
+            with open(output_file, "w") as f:
+                proc = subprocess.Popen(args, stdout=f)  # nosec
+                f.close()
+        except IOError:
+            logging.error(f"Can't open file {output_file} to write.")
+    else:
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # nosec
+        if live_output:
+            if proc.stdout:
+                while True:
+                    output = proc.stdout.readline()
+                    if proc.poll() is not None:
+                        break
+                    if output:
+                        line = output.decode("utf-8").strip()
+                        # Avoid funny ort log output that ressemble markup closing tag
+                        if "[/" in line:
+                            line = line.replace("[", "").replace("]", "")
+                        console.print(line, style="bright_white")
 
     res = proc.wait()
     logging.debug(f"Return code: {res}")
