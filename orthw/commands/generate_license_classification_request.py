d19ec752ce45b1f5ec575b7b4920609d7f39abd5
Helio Chissini de Castro <heliocastro@gmail.com>
2023-03-14 09:42:49 +0100

    feat(commands): Implement command generate-license-classification-request
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- orthw/commands/generate_license_classification_request.py
+++ orthw/commands/generate_license_classification_request.py
@@ -1,24 +1,34 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2023 Helio Chissini de Castro
 
-from rich import print
+import click
 
 from orthw.commandbase import CommandBase, command_group
 
 
-# ----------------------------------
-# Command Line options and arguments
+from orthw.commands.find_license_url import Command as orthw_command
+from orthw.utils import console
+from orthw.templates import render_txt
 
 
-class OrtHWCommand(CommandBase):
+class Command(CommandBase):
     """orthw command - generate-license-classification-request"""
 
     _command_name: str = "generate-license-classification-request"
 
-    def process(self) -> None:
-        print("\n[sandy_brown]This command is not implemented yet.[/sandy_brown]")
+    def process(self, license_id: str, stdout: bool = True) -> str:
+        # Template data to replace
+        data: dict[str, str] = {
+            "license_id": license_id,
+            "license_url": orthw_command().find_license_text_url(license_id),
+        }
+        output = render_txt("license_classification_request", data)
+        if stdout:
+            console.print(output)
+        return output
 
 
 @command_group.command()
-def generate_license_classification_request() -> None:
-    OrtHWCommand().process()
+@click.argument("license-id")
+def generate_license_classification_request(license_id: str) -> None:
+    Command().process(license_id)
